0252c221fbb0817557a178bbc4a4df63f86f16ef
TradeUpCards <coryvandenberg@gmail.com>
2024-08-12 01:49:48 -0500

    Add sigs to process later

--- src/pages/api/missionHandler.ts
+++ src/pages/api/missionHandler.ts
@@ -1,30 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { checkForMissionTrx } from '../../utils/solanaUtils'; // Import the checkForMissionTrx function
+import { supabase } from '../../utils/supabaseClient';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         try {
             const transactions = req.body; // Helius sends an array of transaction signatures
 
-            // Flag to stop checking further signatures once a relevant transaction is found
-            let foundRelevantTransaction = false;
-
             for (const transaction of transactions) {
                 // Iterate over all signatures in the transaction
                 for (const signature of transaction.transaction.signatures) {
                     console.log("Processing signature:", signature);
 
-                    // Check if the signature fits the selected transaction types
-                    const isRelevant = await checkForMissionTrx(signature);
+                    const { error } = await supabase
+                    .from('trx_to_process')
+                    .insert({ signature });
 
-                    if (isRelevant) {
-                        foundRelevantTransaction = true;
-                        break; // Stop checking other signatures if one matches
+                    if (error) {
+                        console.error('Error inserting signature into database:', error);
+                        throw new Error('Database Insertion Error');
                     }
-                }
 
-                if (foundRelevantTransaction) {
-                    break; // Stop checking other transactions if a relevant one is found
+                    console.log(`Inserted signature ${signature} into trx_to_process table`);   
                 }
             }
 
