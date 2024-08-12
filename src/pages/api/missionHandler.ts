7293076ded917ba4136209bc35bcd53e31e4587c
TradeUpCards <coryvandenberg@gmail.com>
2024-08-12 02:13:42 -0500

    blocktime insert

--- src/pages/api/missionHandler.ts
+++ src/pages/api/missionHandler.ts
@@ -5,23 +5,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method === 'POST') {
         try {
             const transactions = req.body; // Helius sends an array of transaction signatures
-console.log("Processing transactions:", transactions);
+            console.log("Processing transactions:", transactions);
+
             for (const transaction of transactions) {
+                // Extract blockTime from the transaction
+                const blockTime = transaction.blockTime;
+
                 // Iterate over all signatures in the transaction
                 for (const signature of transaction.transaction.signatures) {
                     console.log("Processing signature:", signature);
-                    console.log("blocktime:", transaction.blockTime);
+                    console.log("blocktime:", blockTime);
 
                     const { error } = await supabase
-                    .from('trx_to_process')
-                    .insert({ signature });
+                        .from('trx_to_process')
+                        .insert({ signature, blockTime });
 
                     if (error) {
                         console.error('Error inserting signature into database:', error);
                         throw new Error('Database Insertion Error');
                     }
 
-                    console.log(`Inserted signature ${signature} into trx_to_process table`);   
+                    console.log(`Inserted signature ${signature} with blocktime ${blockTime} into trx_to_process table`);
                 }
             }
 
